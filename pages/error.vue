4f060ac0c1d76af015247665e702ca00e8224ac0
Abdurashid <rashidbekkamoldinov@gmail.com.com>
2024-07-07 21:54:44 +0500

    add file

---
+++ pages/error.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mx-auto mt-20 text-center">
+    <h1 class="text-6xl font-bold text-red-500">404</h1>
+    <p class="text-2xl mt-4">Page Not Found</p>
+    <router-link to="/" class="text-blue-500 hover:underline mt-4 block"
+      >Back</router-link
+    >
+  </div>
+</template>
